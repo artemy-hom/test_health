da408f8dc05fccac01bd06c286ac3a3d3596c28f
artemy-hom <107314491+artemy-hom@users.noreply.github.com>
2022-06-17 14:14:54 +0300

    commit: add new module 'time'

--- health_test.py
+++ health_test.py
@@ -1,3 +1,28 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget , QApplication , QPushButton ,QLabel , QVBoxLayout
 
-print('hello world')
+app = QApplication([])
+
+window = QWidget()
+
+window.resize(700,400)
+
+line1 = QVBoxLayout()
+
+window.show()
+
+button1 = QPushButton('начать')
+
+text1 = QLabel('Добро пожаловать в программу по определению состояния здоровья.')
+text2 = QLabel('Данное приложение позволит вам с помощью теста Руфие\n      провести первичную диагностикувашего здоровия.')
+
+
+line1.addWidget(text1, alignment = Qt.AlignCenter)
+line1.addWidget(text2, alignment = Qt.AlignCenter)
+
+line1.addWidget(button1, alignment = Qt.AlignCenter)
+
+
+window.setLayout(line1)
+
+app.exec_()
